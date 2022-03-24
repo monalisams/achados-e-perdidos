1776469d8c4e150625d05c0ebd2584025ea1bfb3
Monalisa Martins <mona.mona3256@gmail.com>
2022-03-24 09:04:41 -0300

    Configuracao banco de dados e adição schema.sql

---
+++ src/main/resources/meu_schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `users` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `full_name` varchar(255) NOT NULL,
+    `email` varchar(255)
+);
+
+CREATE TABLE `item` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255),
+    `description` varchar(150),
+    `status` boolean,
+    `date_item` datetime,
+    `latitude` varchar(25),
+    `longitude` varchar(25),
+    `name_found` varchar(100),
+    `phone` varchar(11),
+    `email` varchar(255),
+    `owner_id` int
+);
+
+CREATE TABLE `owner` (
+    `id` int PRIMARY KEY,
+    `name` varchar(255),
+    `cpf` varchar(11),
+    `birth_date` datetime,
+    `phone` varchar(11),
+    `email` varchar(255),
+    `identification_data` datetime
+);
+
+ALTER TABLE `item` ADD FOREIGN KEY (`owner_id`) REFERENCES `owner` (`id`);
