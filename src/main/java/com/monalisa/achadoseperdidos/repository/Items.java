6d33d1444cdac32e282cbc6e6a0503455f218535
Monalisa Martins <mona.mona3256@gmail.com>
2022-03-26 17:03:54 -0300

    Finalizacao dos repositories

--- src/main/java/com/monalisa/achadoseperdidos/repository/Items.java
+++
@@ -1,8 +0,0 @@
-package com.monalisa.achadoseperdidos.repository;
-
-import com.monalisa.achadoseperdidos.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Items extends JpaRepository<Item, Integer> {
-
-}
--- src/main/java/com/monalisa/achadoseperdidos/repository/Owners.java
+++
@@ -1,7 +0,0 @@
-package com.monalisa.achadoseperdidos.repository;
-
-import com.monalisa.achadoseperdidos.entity.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Owners extends JpaRepository<Owner, Integer> {
-}
