b8c6a8ebc8d1e86b6d04a77630e058b079b959c5
Monalisa Martins <mona.mona3256@gmail.com>
2022-03-26 17:05:21 -0300

    Criacao dos Controllers

---
+++ src/main/java/com/monalisa/achadoseperdidos/controller/OwnerController.java
@@ -0,0 +1,60 @@
+package com.monalisa.achadoseperdidos.controller;
+
+import com.monalisa.achadoseperdidos.dto.OwnerDTO;
+import com.monalisa.achadoseperdidos.entity.Owner;
+import com.monalisa.achadoseperdidos.service.OwnerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/owner")
+@RequiredArgsConstructor
+public class OwnerController {
+    private final OwnerService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Owner saveOwner(@RequestBody OwnerDTO ownerDTO) {
+        return service.saveOwner(ownerDTO);
+    }
+
+    @DeleteMapping("/id")
+    public void deleteOwner(@PathVariable Long id){
+        service
+        .deleteOwner(id)
+        .orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Owner não encontrado."));
+    }
+
+    @PutMapping("/{id}")
+    public Owner updateOwner(@PathVariable Long id, @RequestBody OwnerDTO ownerDTO){
+        return  service
+                .updateOwner(id, ownerDTO)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Owner não encontrado."));
+
+    }
+
+    @GetMapping("/{id}")
+    public Owner getOwner(@PathVariable Long id){
+        return  service
+                .getOwnerId(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Owner não encontrado."));
+    }
+
+    @GetMapping("/all")
+    public List<Owner> getOwners(){
+        return service
+                .getOwners();
+    }
+
+    @GetMapping
+    public List<Owner> getOwner(OwnerDTO filter){
+        return service
+                .getOwners(filter);
+    }
+}
